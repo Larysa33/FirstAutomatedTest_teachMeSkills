4bac210ef0d9291a52f4235557b0a277731c84d4
Larysa33 <ligorev@yahoo.com>
2023-02-13 21:17:41 +0300

    Renaming constants.

--- src/test/java/by/teachmeskills/hometask13/TaxTest.java
+++ src/test/java/by/teachmeskills/hometask13/TaxTest.java
@@ -4,24 +4,22 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
 
-import java.util.Scanner;
-
 /*Вычислить сумму налога по прогрессивной шкале в зависимости от полученного заработка: 13% на доход до 10 000,
 20% на доход от 10 000 до 50 000, 30% на доход выше 50 000.
 Метод должен принимать сумму оплаты труда, а возвращать сумму налога.*/
 public class TaxTest {
-    public static final double LOWESTPERCENT = 0.13;
-    public static final double MEDIUMPERCENT = 0.20;
-    public static final double HIGHESTPERCENT = 0.30;
+    public static final double LOWEST_PERCENT = 0.13;
+    public static final double MEDIUM_PERCENT = 0.20;
+    public static final double HIGHEST_PERCENT = 0.30;
 
     public double countTax(double salary) {
         double tax;
         if (salary < 10000) {
-            tax = salary * LOWESTPERCENT;
+            tax = salary * LOWEST_PERCENT;
         } else if (salary >= 10000 && salary <= 50000) {
-            tax = salary * MEDIUMPERCENT;
+            tax = salary * MEDIUM_PERCENT;
         } else {
-            tax = salary * HIGHESTPERCENT;
+            tax = salary * HIGHEST_PERCENT;
         }
         System.out.println("Процент от Вашей зарплаты составляет: " + tax);
         return tax;
